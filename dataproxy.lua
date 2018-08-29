211440c1d58ec6e40086f598b775c9f3f1b2150f
CetoBasilius <basiliogerman@gmail.com>
2018-08-29 18:27:11 -0500

    Changed the way dataproxy works as Android failed with metatables on userdata

--- dataproxy.lua
+++ dataproxy.lua
@@ -3,22 +3,73 @@ local dataproxy = {}
 -------------------------------------------- Constants
 -------------------------------------------- Caches
 -------------------------------------------- Vars
+local proxyList
+local initialized
 -------------------------------------------- Local functions
+local function initialize()
+	if not initialized then
+		initialized = true
+		
+		proxyList = {}
+	end
+end
+
+local function findProxy(userdata)
+	local foundProxy = proxyList[userdata] -- Quick search
+	if not foundProxy then -- Search through iteration
+		for key, proxy in pairs(proxyList) do
+			if key == userdata then
+				foundProxy = proxy
+				
+				break
+			end
+		end
+	end
+	
+	return foundProxy
+end
+
+local function removeProxy(userdata)
+	local foundProxy = proxyList[userdata] -- Quick search
+	if not foundProxy then -- Search through iteration
+		for key, proxy in pairs(proxyList) do
+			if key == userdata then
+				proxyList[key] = nil
+				
+				return true
+			end
+		end
+	else
+		proxyList[userdata] = nil
+		
+		return true
+	end
+	
+	return false
+end
 -------------------------------------------- Module functions
-function dataproxy.set(userdata)
+function dataproxy.get(userdata)
 	if type(userdata) ~= "userdata" then
 		error("Expected userdata", 2)
 	end
 	
-	local meta = getmetatable(userdata)
-	local proxy = {}
-	meta.__index = function(self, key)
-		return proxy[key]
+	local proxy = findProxy(userdata)
+	if not proxy then -- No proxy, create new and index
+		proxy = {}
+		proxyList[userdata] = proxy
 	end
-	meta.__newindex = function(self, key, value)
-		proxy[key] = value
+	
+	return proxy
+end
+
+function dataproxy.remove(userdata)
+	if type(userdata) ~= "userdata" then
+		error("Expected userdata", 2)
 	end
+	
+	return removeProxy(userdata)
 end
 -------------------------------------------- Execution
+initialize()
 
 return dataproxy
--- main.lua
+++ main.lua
@@ -1,12 +1,20 @@
+require("mobdebug").start()
 -------------------------------------------- Main
 local dataproxy = require("dataproxy")
 
 local function networkListener(event)
 	local handle = event.requestId
+	local proxy = dataproxy.get(handle)
 	
-	print(handle.something)
+	print(proxy.something)
+	
+	dataproxy.remove(handle)
 end
 
-local handle = network.request("http://127.0.0.1/", "GET", networkListener)
-dataproxy.set(handle)
-handle.something = "super"
+local handle1 = network.request("http://127.0.0.1/", "GET", networkListener)
+local proxy1 = dataproxy.get(handle1)
+proxy1.something = "super"
+
+local handle2 = network.request("http://127.0.0.1/", "GET", networkListener)
+local proxy2 = dataproxy.get(handle2)
+proxy2.something = "nice"
